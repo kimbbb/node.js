f6084d69ddd0e16c8d751b60dbf181406952f443
김민지 <wehouse201@naver.com>
2023-05-01 20:55:27 +0900

    homeworkk

--- mongo/schemas/index.js
+++ mongo/schemas/index.js
@@ -11,17 +11,19 @@ const connect=async()=>{
   catch{
 
   }
-  mongoose.connect('mongodb://root:nodejsbook@localhost:27017/admin',{
-    dbName:'nodejs',
+  mongoose.connect('mongodb://kimminji:*****@localhost:27017/?authMechanism=DEFAULT',{
+    dbName:'kimminji',
     useNewUrlParser:true,
   }).then(()=>{
     console.log("몽고디비 연결 성공");
   }).catch((err)=>{
     console.error('몽고디비 연결 에러', err);
   });
-
-  
 };
+
+mongoose.connection.on('error', (error)=>{
+  console.error('몽고디비 연결 에러', error);
+});
 mongoose.connection.on('disconnected', ()=>{
   console.error('몽고디비 연결이 끊겼습니다. 연결을 재시도 합니다.');
   connect();
